1f47ff50bd39c20f25f063041564a8c05c353a96
Jason de Carvalho <j.decarvalho@ocado.com>
2015-08-13 12:29:26 +0100

    Switch to vertx-web.

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -2,19 +2,31 @@ package io.vertx.blog.first;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
 	@Override
 	public void start(Future<Void> future) {
+		Router router = Router.router(vertx);
+		bindHelloMessage(router);
+		createServer(future, router);
+	}
+
+	private void bindHelloMessage(Router router) {
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response
+					.putHeader("content-type", "text/html")
+					.end("<h1>Hello from my first Vert.x 3 application</h1>");
+		});
+	}
+
+	private void createServer(Future<Void> future, Router router) {
 		vertx
 				.createHttpServer()
-				.requestHandler(r ->
-								r.response().end(
-										"<h1>Hello from my first " +
-												"Vert.x 3 application</h1>"
-								)
-				)
+				.requestHandler(router::accept)
 				.listen(
 						config().getInteger("http.port", 8080),
 						result -> {
@@ -23,6 +35,7 @@ public class MyFirstVerticle extends AbstractVerticle {
 							} else {
 								future.fail(result.cause());
 							}
-						});
+						}
+				);
 	}
 }
